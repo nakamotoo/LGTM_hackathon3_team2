25abe82beeb11b69e3b36259193a284df41571eb
nakamoto <mitsuhiko0820.acm@gmail.com>
2019-02-19 16:43:57 +0900

    fix reducer

--- src/reducers/MaterialReducer.js
+++ src/reducers/MaterialReducer.js
@@ -2,20 +2,20 @@ import * as actionType from "../actions/types";
 
 const INITIAL_STATE = {
   food: [
-    { id: 1, name: "キャベツ", value: 100 },
-    { id: 2, name: "にんじん", value: 120 },
-    { id: 3, name: "豚肉", value: 100 },
-    { id: 4, name: "df", value: 100 },
-    { id: 5, name: "676e5", value: 100 },
-    { id: 6, name: "ddd", value: 100 },
-    { id: 7, name: "めん", value: 100 }
+    { foodId: 1, name: "キャベツ", value: 100 },
+    { foodId: 2, name: "にんじん", value: 120 },
+    { foodId: 3, name: "豚肉", value: 100 },
+    { foodId: 4, name: "df", value: 100 },
+    { foodId: 5, name: "676e5", value: 100 },
+    { foodId: 6, name: "ddd", value: 100 },
+    { foodId: 7, name: "めん", value: 100 }
   ],
   tool: [
-    { id: 1, name: "鍋", value: 100 },
-    { id: 2, name: "紙皿", value: 120 },
-    { id: 3, name: "おわん", value: 100 },
-    { id: 4, name: "コップ", value: 100 },
-    { id: 5, name: "おたま", value: 100 }
+    { toolId: 1, name: "鍋", value: 100 },
+    { toolId: 2, name: "紙皿", value: 120 },
+    { toolId: 3, name: "おわん", value: 100 },
+    { toolId: 4, name: "コップ", value: 100 },
+    { toolId: 5, name: "おたま", value: 100 }
   ]
 };
 
