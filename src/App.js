9983a246bf728f5d27225c341d83026164a21dd0
nakamoto <mitsuhiko0820.acm@gmail.com>
2019-02-19 14:47:00 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import Router from "./Router";
 
 import MainComponent from "./components/main";
 
@@ -8,7 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <MainComponent />
+        <Router />
       </Provider>
     );
   }
