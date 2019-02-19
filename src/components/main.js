cda6c5212b9d8018a860683d70dd099b6a824c84
Kenta Ichimura <kenta100718@gmail.com>
2019-02-19 15:55:52 +0900

    flatlist

--- src/components/main.js
+++ src/components/main.js
@@ -1,18 +1,67 @@
 import React, { Component } from "react";
-import { Text, View, TextInput, StyleSheet, Platform } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  Platform,
+  FlatList,
+  ScrollView,
+  Dimensions,
+  Image
+} from "react-native";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 import { Actions } from "react-native-router-flux";
 
 class MainContainer extends Component {
+  renderItem = list => {
+    return (
+      <View style={styles.listContainer}>
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={{
+            uri: list.item.image
+          }}
+        />
+        <Text>{list.item.name}</Text>
+      </View>
+    );
+  };
   render() {
+    const imageArray = {
+      hinabe:
+        "https://www.marukome.co.jp/files/recipe/3445/5ab85c53-66b4-4cb2-a24c-a4b91b85f192.jpg",
+      motsunabe:
+        "https://park.ajinomoto.co.jp/wp-content/uploads/2018/03/710648.jpeg",
+      misonabe:
+        "https://park.ajinomoto.co.jp/wp-content/uploads/2018/03/710396.jpeg",
+      gekikaranabe:
+        "https://trendy.nikkeibp.co.jp/article/pickup/20150219/1062785/thumb_400_07_px400.jpg"
+    };
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions} onPress={() => Actions.orderScreen()}>
-          {this.props.testId}
-        </Text>
+        <ScrollView>
+          <FlatList
+            data={[
+              { key: "a", name: "四川風火鍋", image: imageArray.hinabe },
+              { key: "b", name: "もつ鍋", image: imageArray.motsunabe },
+              { key: "c", name: "味噌鍋", image: imageArray.misonabe },
+              { key: "d", name: "激辛", image: imageArray.gekikaranabe }
+            ]}
+            contentContainerStyle={styles.flatListContainer}
+            numColumns={2}
+            renderItem={this.renderItem}
+          />
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.js</Text>
+          <Text
+            style={styles.instructions}
+            onPress={() => Actions.orderScreen()}
+          >
+            {this.props.testId}
+          </Text>
+        </ScrollView>
       </View>
     );
   }
@@ -29,6 +78,8 @@ export default connect(
   actions
 )(MainContainer);
 
+const primary_side_margin = 4;
+const list_item_side_margin = 4;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -45,5 +96,22 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#333333",
     marginBottom: 5
+  },
+  listContainer: {
+    width:
+      (Dimensions.get("window").width -
+        (list_item_side_margin * 4 + primary_side_margin * 2)) /
+      2,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 4
+  },
+  flatListContainer: {
+    backgroundColor: "red",
+    marginLeft: primary_side_margin,
+    marginRight: primary_side_margin,
+    marginTop: primary_side_margin,
+    alignItems: "flex-start",
+    justifyContent: "center"
   }
 });
